b5fe2a1a0bc5b59939b920657e31d5f5628cf2f7
Marcos Y. <`[marcos@wazoom-studio.com](mailto:marcos@wazoom-studio.com)`>
2024-12-07 07:28:56 +0100

    refactor : export img path

---
+++ src/assets/img/index.ts
@@ -0,0 +1,17 @@
+import face from "./icon/face.svg";
+import insta from "./icon/insta.svg";
+import logo from "./icon/logo.svg";
+import plat from "./plat.png";
+import recipe from "./icon/recipe.svg";
+import toggle from "./icon/toggle.svg";
+import twitter from "./icon/twitter.svg";
+
+export const images = {
+  face,
+  insta,
+  logo,
+  plat,
+  recipe,
+  toggle,
+  twitter,
+};
