665d496d6b9c47090ce39e8d447c2a7434ecf342
Marcos Y. <`[marcos@wazoom-studio.com](mailto:marcos@wazoom-studio.com)`>
2024-12-09 18:43:48 +0100

    refactor: add asset for recipe list

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,17 +1,67 @@
+import avatar from "./avatar.png";
+import badge from "./Badge.png";
+import burger from "./burger.png";
+import chef from "./chef.png";
+import chicken from "./chicken.png";
+import chickenRice from "./chicken-rice.png";
+import chocolate from "./icon/chocolat.svg";
+import crepe from "./crepe.png";
+import dessert from "./icon/dessert.svg";
 import face from "./icon/face.svg";
+import fish from "./fish.png";
+import forknife from "./icon/ForkKnife.svg";
 import insta from "./icon/insta.svg";
 import logo from "./icon/logo.svg";
+import lunch from "./icon/lunch.svg";
+import meat from "./icon/meat.svg";
+import meatPatato from "./meatPatato.png";
+import onion from "./icon/onion.svg";
+import pancake from "./pancake.png";
+import pasta from "./pasta.png";
 import plat from "./plat.png";
+import play from "./icon/play.svg";
 import recipe from "./icon/recipe.svg";
+import rice from "./icon/rice.svg";
+import salade from "./salade.png";
+import timer from "./icon/Timer.svg";
 import toggle from "./icon/toggle.svg";
+import tomato from "./icon/tomato.svg";
 import twitter from "./icon/twitter.svg";
+import vegan from "./icon/vegan.svg";
+import vegetable from "./vegetables.png";
+import vegetable2 from "./vegetable2.png";
 
 export const images = {
+  avatar,
+  badge,
+  burger,
+  chef,
+  chicken,
+  chickenRice,
+  chocolate,
+  crepe,
+  dessert,
   face,
+  fish,
+  forknife,
   insta,
   logo,
+  lunch,
+  meat,
+  meatPatato,
+  onion,
+  pancake,
+  pasta,
   plat,
+  play,
   recipe,
+  rice,
+  salade,
+  timer,
   toggle,
+  tomato,
   twitter,
+  vegan,
+  vegetable,
+  vegetable2,
 };
