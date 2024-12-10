50bc2e51f1213b6376225beb9d487380fc6a8737
Marcos Y. <`[marcos@wazoom-studio.com](mailto:marcos@wazoom-studio.com)`>
2024-12-10 13:27:00 +0100

    refactor : blog asset

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,25 +1,33 @@
 import avatar from "./avatar.png";
 import badge from "./Badge.png";
+import bolognaise from "./bolognaise.png";
 import burger from "./burger.png";
 import chef from "./chef.png";
 import chicken from "./chicken.png";
 import chickenRice from "./chicken-rice.png";
 import chocolate from "./icon/chocolat.svg";
 import crepe from "./crepe.png";
+import communication from "./communication.png";
 import dessert from "./icon/dessert.svg";
 import face from "./icon/face.svg";
 import fish from "./fish.png";
+import fruitRecipe from "./fruit-recipe.png";
 import forknife from "./icon/ForkKnife.svg";
+import fullGuide from "./full-guide.png";
 import insta from "./icon/insta.svg";
+import lasane from "./lasane.png";
 import logo from "./icon/logo.svg";
 import lunch from "./icon/lunch.svg";
 import meat from "./icon/meat.svg";
 import meatPatato from "./meatPatato.png";
+import noodle from "./noodle.png";
 import onion from "./icon/onion.svg";
 import pancake from "./pancake.png";
 import pasta from "./pasta.png";
 import plat from "./plat.png";
+import plantain from "./plantain.png";
 import play from "./icon/play.svg";
+import pork from "./pork.png";
 import recipe from "./icon/recipe.svg";
 import rice from "./icon/rice.svg";
 import salade from "./salade.png";
@@ -35,25 +43,33 @@ export const images = {
   avatar,
   badge,
   burger,
+  bolognaise,
   chef,
   chicken,
   chickenRice,
   chocolate,
   crepe,
+  communication,
   dessert,
   face,
   fish,
+  fruitRecipe,
   forknife,
+  fullGuide,
   insta,
   logo,
   lunch,
+  lasane,
   meat,
   meatPatato,
+  noodle,
   onion,
   pancake,
   pasta,
   plat,
+  plantain,
   play,
+  pork,
   recipe,
   rice,
   salade,
