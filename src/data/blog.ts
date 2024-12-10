af87ab612a128a0b5c18ba5fd9f92e098b42ff4b
Marcos Y. <`[marcos@wazoom-studio.com](mailto:marcos@wazoom-studio.com)`>
2024-12-10 13:29:54 +0100

    feat : data for list recipe and article

---
+++ src/data/blog.ts
@@ -0,0 +1,72 @@
+import { images } from "@/assets/img";
+
+const articles = [
+  {
+    title: "Crochet Projects for Noodle Lovers",
+    image: images.noodle,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Wade Warren",
+    date: "12 November 2021",
+  },
+  {
+    title: "10 Vegetarian Recipes To Eat This Month",
+    image: images.fruitRecipe,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Robert Fox",
+    date: "12 November 2021",
+  },
+  {
+    title: "Full Guide to Becoming a Professional Chef",
+    image: images.fullGuide,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Dianne Russell",
+    date: "12 November 2021",
+  },
+  {
+    title: "Simple & Delicious Vegetarian Lasagna",
+    image: images.lasane,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Leslie Alexander",
+    date: "12 November 2021",
+  },
+  {
+    title: "Plantain and Pinto Stew with Aji Verde",
+    image: images.plantain,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Courtney Henry",
+    date: "12 November 2021",
+  },
+  {
+    title: "We’re Hiring a Communications Assistant!",
+    image: images.communication,
+    describe:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim",
+    name: "Albert Flores",
+    date: "12 November 2021",
+  },
+];
+
+const tasty = [
+  {
+    title: "Chicken Meatballs with Cream Cheese",
+    image: images.chicken,
+    name: "Andreas Paula",
+  },
+  {
+    title: "Traditional Bolognaise Ragu",
+    image: images.bolognaise,
+    name: "By Andreas Paula",
+  },
+  {
+    title: "Pork and Chive Chinese Dumplings",
+    image: images.pork,
+    name: "Andreas Paula",
+  },
+];
+
+export { articles, tasty };
---
+++ src/data/home.ts
@@ -0,0 +1,140 @@
+import { images } from "@/assets/img";
+
+const menuItem = [
+  {
+    title: "Home",
+    link: "/home",
+  },
+  {
+    title: "Recipes",
+    link: "/recipes",
+  },
+  {
+    title: "Blog",
+    link: "blog",
+  },
+  // {
+  //   title: "Contact",
+  //   link: "#",
+  // },
+  // {
+  //   title: "About us",
+  //   link: "#",
+  // },
+];
+
+const categorie = [
+  {
+    name: "Breakfast",
+    image: images.rice,
+    link: "#",
+    class: "background-rice",
+  },
+  {
+    name: "Vegan",
+    image: images.vegan,
+    link: "#",
+    class: "background-vegan",
+  },
+  {
+    name: "Meat",
+    image: images.meat,
+    link: "#",
+    class: "background-meat",
+  },
+  {
+    name: "Dessert",
+    image: images.dessert,
+    link: "#",
+    class: "background-dessert",
+  },
+  {
+    name: "Lunch",
+    image: images.lunch,
+    link: "#",
+    class: "background-lunch",
+  },
+  {
+    name: "Lunch",
+    image: images.chocolate,
+    link: "#",
+    class: "background-meat",
+  },
+];
+
+const recipes = [
+  {
+    title: "Spicy delicious chicken wings",
+    image: images.burger,
+    name: "burger",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "Chicken Meatballs with Cream Cheese",
+    image: images.chicken,
+    name: "chicken",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "The Best Easy One Pot Chicken and Rice",
+    image: images.chickenRice,
+    name: "chickenRice",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "Strawberry Oatmeal Pancake with Honey Syrup",
+    image: images.crepe,
+    name: "crepe",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "Fresh Lime Roasted Salmon with Ginger Sauce",
+    image: images.fish,
+    name: "fish",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "Fruity Pancake with Orange & Blueberry",
+    image: images.pancake,
+    name: "pancake",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "The Creamiest Creamy Chicken and Bacon Pasta",
+    image: images.pasta,
+    name: "pasta",
+    time: "30 Minutes",
+    servings: "4 Chicken",
+    link: "#",
+  },
+  {
+    title: "Fresh and Healthy Mixed Mayonnaise Salad",
+    image: images.salade,
+    name: "salade",
+    time: "30 Minutes",
+    servings: "4 Salad",
+    link: "#",
+  },
+  {
+    title: "Steak Meatballs with potato",
+    image: images.meatPatato,
+    name: "Steak",
+    time: "30 Minutes",
+    servings: "4 Steak",
+    link: "#",
+  },
+];
+
+export { categorie, menuItem, recipes };
